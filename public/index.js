23b0d7f91424a2b1817a234dc57339f86d1bf81e
Giulia <teggi.giulia@gmail.com>
2018-05-09 16:58:35 +0100

    Update spacing

--- public/index.js
+++ public/index.js
@@ -6,33 +6,33 @@ var buttonTryMe = document.getElementById('button__tryme');
  * Checks if there is a service worker and registers it
  */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then(function () { console.log("Service Worker Registered"); });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () { console.log("Service Worker Registered"); });
 }
 
 buttonTryMe.addEventListener('click', function () {
-    clearContents();
-    generateGiftPage();
+  clearContents();
+  generateGiftPage();
 });
 
 /**
  * Clears the content of index.html by removing the children of <main>
  */
 var clearContents = function () {
-    while (main.firstChild) {
-        main.removeChild(main.firstChild);
-}
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
 
-/**
- * Generates the random gift page building all necessary elements
- */
-var generateGiftPage = function () {
+  /**
+   * Generates the random gift page building all necessary elements
+   */
+  var generateGiftPage = function () {
     var hello = document.createElement('p');
     var helloText = document.createTextNode('I am the gift page');
     hello.appendChild(helloText);
     main.appendChild(hello);
-}
+  }
 
 
 
