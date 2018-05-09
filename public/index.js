1b843864285a886d3d4a45a3d27cd656275d78fa
Giulia <teggi.giulia@gmail.com>
2018-05-09 14:45:10 +0100

    Add comments to functions

--- public/index.js
+++ public/index.js
@@ -2,12 +2,24 @@
 var main = document.querySelector('main'); 
 var buttonTryMe = document.getElementById('button__tryme');
 
+
+buttonTryMe.addEventListener('click', function(){
+    clearContents(); 
+    generateGiftPage(); 
+}); 
+
+/**
+ * Clears the content of index.html by removing the children of <main>
+ */
 var clearContents = function(){
     while(main.firstChild){
         main.removeChild(main.firstChild);
     }
 }
 
+/**
+ * Generates the random gift page building all necessary elements
+ */
 var generateGiftPage = function(){
     var hello = document.createElement('p'); 
     var helloText = document.createTextNode('I am the gift page'); 
@@ -15,8 +27,7 @@ var generateGiftPage = function(){
     main.appendChild(hello); 
 }
 
-buttonTryMe.addEventListener('click', function(){
-    clearContents(); 
-    generateGiftPage(); 
-}); 
+
+
+
 
