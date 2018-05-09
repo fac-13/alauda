ee5287b602d8dcbff8c27a8e7f02233be861cd7a
Parissai <parissa.jamali@gmail.com>
2018-05-09 15:02:23 +0100

    add / rout test Relates #24

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
-const router = require('../controllers/index');
-console.log(router);
+const app = require('../../src/app')
+
 describe('Test the root path', () => {
-  test('It should response the GET method', () =>
-    request(router)
-      .get('/')
-      .expect(200));
-});
+  test('It should response the GET method', () => {
+    return request(app).get("/").then(response => {
+      expect(response.statusCode).toBe(200)
+    })
+  });
+})
