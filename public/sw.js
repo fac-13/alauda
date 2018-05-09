390a52232a28002cd2ebe46bd57e15d85b222a55
Giulia <teggi.giulia@gmail.com>
2018-05-09 16:42:43 +0100

    Create manifest.json

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,15 @@
 const staticAssets = [
-  "./",
-  "./style.css",
-  "./index.js",
+  './',
+  './style.css',
+  './index.js',
+  './images/icons/icon-72x72.png',
+  './images/icons/icon-96x96.png',
+  './images/icons/icon-128x128.png',
+  './images/icons/icon-144x144.png',
+  './images/icons/icon-152x152.png',
+  './images/icons/icon-192x192.png',
+  './images/icons/icon-384x384.png',
+  './images/icons/icon-512x512.png', 
 ];
 
 
@@ -10,8 +18,13 @@ self.addEventListener('install', async event => {
   cache.addAll(staticAssets);
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  console.log("Req:", req);
+  event.respondWith(cacheFirst(req));
+});
 
-  console.log(event.request.url);
-  
-  });
+async function cacheFirst(req) {
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
+}
