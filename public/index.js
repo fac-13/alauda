8fcee3217c4b57b980ea5e02fa9279185ae3df7c
Parissai <parissa.jamali@gmail.com>
2018-05-10 12:48:14 +0100

    add comment for checkOffline function Relates #30

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ var main = document.querySelector('main');
 var buttonTryMe = document.getElementById('button__tryme');
 
 /**
- * Checks if there is a service worker and registers it
+ * Check if there is a service worker and register it
  */
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
@@ -17,7 +17,7 @@ buttonTryMe.addEventListener('click', function () {
 });
 
 /**
- * Clears the content of index.html by removing the children of <main>
+ * Clear the content of index.html by removing the children of <main>
  */
 var clearContents = function () {
   while (main.firstChild) {
@@ -26,7 +26,7 @@ var clearContents = function () {
 }
 
 /**
- * Generates the random gift page building all necessary elements
+ * Generate the random gift page building all necessary elements
  */
 var generateGiftPage = function () {
   var hello = document.createElement('p');
@@ -40,6 +40,9 @@ var generateGiftPage = function () {
   giftButton.addEventListener('click', checkOffline)
 }
 
+/**
+ * Check if user is offline, open dialog box
+ */
 var checkOffline = function () {
   if (!navigator.onLine) {
     var offlineMessage = document.createElement('dialog');
