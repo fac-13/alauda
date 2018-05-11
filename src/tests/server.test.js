1e9bd97210ec64a37617cff6e14421a0fa8fc8c5
missKatiaPunter <katiapunter@gmail.com>
2018-05-11 15:19:53 +0100

    adds tests for home and try routes

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,12 +1,24 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
-describe('Test the root path', () => {
-  test('It should response the GET method', () =>
+describe('Test the home path', () => {
+  test('It should send the response after the GET method executes', () =>
     request(app)
       .get('/')
-      .then(response => {
+      .then((response) => {
         expect(response.statusCode).toBe(200);
+      }));
+  test('It should produce a page with a button that says Try Me', () =>
+    request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.text.includes('<a href="/try">Try Me</a>')).toBe(true);
+      }));
+  test('It should produce a page with a button that has id button__tryMe', () =>
+    request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.text.includes('<button id="button__tryMe">')).toBe(true);
       }));
 });
 
@@ -14,8 +26,26 @@ describe('Test the api path', () => {
   test('It should send the content object', () =>
     request(app)
       .get('/api/content')
-      .then(response => {
+      .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(typeof response).toBe('object');
       }));
 });
+
+describe('Test the try path', () => {
+  test('It should send the content object', () =>
+    request(app)
+      .get('/try')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(typeof response).toBe('object');
+      }));
+  test('It should send the content object', () =>
+    request(app)
+      .get('/try')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.text.includes('<!DOCTYPE html>')).toBe(true);
+      }));
+});
+
