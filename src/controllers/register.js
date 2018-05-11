47062baf872a063da672adafbe4e4f0efd89af9c
missKatiaPunter <katiapunter@gmail.com>
2018-05-11 16:46:53 +0100

    adds text to home page, login, sign up links Relates #6

---
+++ src/controllers/register.js
@@ -0,0 +1,5 @@
+exports.get = (req, res) => {
+  console.log('Sign up login is reached');
+
+  res.render('register');
+};
--- src/controllers/router.js
+++ src/controllers/router.js
@@ -5,29 +5,29 @@ const path = require('path');
 const router = express.Router();
 
 const home = require('./home');
-const randomGift = require('./randomGift'); 
-const randomContent = require('./randomContent'); 
+const randomGift = require('./randomGift');
+const randomContent = require('./randomContent');
+const register = require('./register');
 
 const content = {
   music: {},
   images: {},
-  placeholder: []
+  placeholder: [],
 };
 
 const placeholderUrl =
   'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC';
 
-  
-router.get('/', home.get); 
-router.get('/try', randomGift.get); 
-router.get('/randomContent', randomContent.get); 
+
+router.get('/', home.get);
+router.get('/try', randomGift.get);
+router.get('/randomContent', randomContent.get);
+router.get('/register', register.get);
 
 router.get('/api/content', (req, res) => {
   fetch(placeholderUrl)
-    .then(response => {
-      return response.json();
-    })
-    .then(json => {
+    .then(response => response.json())
+    .then((json) => {
       const data = json.data;
       content.placeholder = data.map(el => el.images.original.url);
       res.send(content);
