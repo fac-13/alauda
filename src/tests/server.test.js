6d51b99ea256eba9da5a63980011274c9d4798ba
missKatiaPunter <katiapunter@gmail.com>
2018-05-11 15:43:04 +0100

    adds tests for home and try routes Relates #24

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -2,13 +2,19 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('Test the home path', () => {
-  test('It should send the response after the GET method executes', () =>
+  test('It should send the response after the GET method executes with status code 200', () =>
     request(app)
       .get('/')
       .then((response) => {
         expect(response.statusCode).toBe(200);
       }));
-  test('It should produce a page with a button that says Try Me', () =>
+  test('It should send an html page', () =>
+    request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.type).toBe('text/html');
+      }));
+  test('It should produce a page with a link that says Try Me', () =>
     request(app)
       .get('/')
       .then((response) => {
@@ -27,25 +33,28 @@ describe('Test the api path', () => {
     request(app)
       .get('/api/content')
       .then((response) => {
-        expect(response.statusCode).toBe(200);
         expect(typeof response).toBe('object');
       }));
 });
 
 describe('Test the try path', () => {
-  test('It should send the content object', () =>
+  test('It should send the response after the GET method executes with status code 200', () =>
     request(app)
       .get('/try')
       .then((response) => {
         expect(response.statusCode).toBe(200);
-        expect(typeof response).toBe('object');
       }));
-  test('It should send the content object', () =>
+  test('It should have a section', () =>
     request(app)
       .get('/try')
       .then((response) => {
-        expect(response.statusCode).toBe(200);
-        expect(response.text.includes('<!DOCTYPE html>')).toBe(true);
+        expect(response.text.includes('<section>')).toBe(true);
+      }));
+  test('It should send an html page', () =>
+    request(app)
+      .get('/try')
+      .then((response) => {
+        expect(response.type).toBe('text/html');
       }));
 });
 
