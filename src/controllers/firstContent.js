802537bf399872b803cb7a268143d7a8b149f823
isnotafunction <ivana.cosgrove@gmail.com>
2018-05-13 21:39:23 +0100

    styles articles and smooth appearance of all content

--- src/controllers/firstContent.js
+++ src/controllers/firstContent.js
@@ -1,7 +1,7 @@
 require('env2')('.env');
 const fetch = require('node-fetch');
 
-const query = 'first';
+const query = 'good';
 const newsUrl = `https://newsapi.org/v2/everything?q=${query}&totalResults=10&apiKey=${process.env.NEWS_KEY}`;
 const giphyUrl = 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC';
 exports.get = async (req, res) => {
