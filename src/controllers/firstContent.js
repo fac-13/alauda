fdb5790038b796dd517cb7d92f4fd55fae6da197
isnotafunction <ivana.cosgrove@gmail.com>
2018-05-13 14:32:48 +0100

    add multiple api calls (needs error handling)

--- src/controllers/firstContent.js
+++ src/controllers/firstContent.js
@@ -3,10 +3,11 @@ const fetch = require('node-fetch');
 
 const query = 'first';
 const newsUrl = `https://newsapi.org/v2/everything?q=${query}&totalResults=10&apiKey=${process.env.NEWS_KEY}`;
-
-exports.get = (req, res) => {
-  fetch(newsUrl)
-    .then(response => response.json())
-    .then(json => json.articles)
-    .then(data => res.send(data));
+const giphyUrl = 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC';
+exports.get = async (req, res) => {
+  const responses = await Promise.all([
+    fetch(newsUrl).then(data => data.json()).then(data => data.articles),
+    fetch(giphyUrl).then(data => data.json()).then(json => json.data),
+  ]);
+  res.send(responses);
 };
