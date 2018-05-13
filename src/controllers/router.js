9bcb0d96054a0f729456b6595d6db6aba09db0f0
Parissai <parissa.jamali@gmail.com>
2018-05-13 16:38:42 +0100

    add signup form

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -7,11 +7,13 @@ const randomGift = require('./randomGift');
 const randomContent = require('./randomContent');
 const firstContent = require('./firstContent');
 const regularContent = require('./regularContent');
+const signup = require('./signup');
 
 router.get('/', home.get);
 router.get('/try', randomGift.get);
 router.get('/randomContent', randomContent.get);
 router.get('/api/firstContent', firstContent.get);
 router.get('api/content', regularContent.get);
+router.get('/signup', signup.get);
 
 module.exports = router;
---
+++ src/controllers/signup.js
@@ -0,0 +1,4 @@
+exports.get = (req, res) => {
+  console.log('signup.js is reached');
+  res.render('signup');
+};
