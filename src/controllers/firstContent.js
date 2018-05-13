01fdf7b50a135dd08e955f3e30b9993d56deda46
isnotafunction <ivana.cosgrove@gmail.com>
2018-05-13 11:15:50 +0100

    fix async sending data

--- src/controllers/firstContent.js
+++ src/controllers/firstContent.js
@@ -4,21 +4,9 @@ const fetch = require('node-fetch');
 const query = 'first';
 const newsUrl = `https://newsapi.org/v2/everything?q=${query}&totalResults=10&apiKey=${process.env.NEWS_KEY}`;
 
-const content = {
-  news: [],
-};
-/**
- * @param  {} url; make an API call
- */
-const fetchApi = (url) => {
-  fetch(url)
-    .then(response => response.json())
-    .then((json) => {
-      content.news = json.articles;
-    });
-};
-
 exports.get = (req, res) => {
-  fetchApi(newsUrl);
-  res.send(content);
+  fetch(newsUrl)
+    .then(response => response.json())
+    .then(json => json.articles)
+    .then(data => res.send(data));
 };
