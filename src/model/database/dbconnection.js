31a2e3f6077aff3a610871c6ac93489e345a8463
missKatiaPunter <katiapunter@gmail.com>
2018-05-14 14:13:00 +0100

    creates dbconnection file

---
+++ src/model/database/dbconnection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+require('env2')('./env');
+
+let dbconnect = process.env.DATABASE_URL;
+
+if (!dbconnect) {
+  throw new Error('Environment variable DATABASE_URL should be set');
+}
+if (process.env.NODE_ENV === 'test') {
+  dbconnect = process.env.TEST_URL;
+}
+
+mongoose.connect(dbconnect);
