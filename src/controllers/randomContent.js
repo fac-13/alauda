cb002a6bdfc5d84a65e7d843d0ff424fd8c9873b
Parissai <parissa.jamali@gmail.com>
2018-05-16 21:28:35 +0100

    change ink to backLink

--- src/controllers/randomContent.js
+++ src/controllers/randomContent.js
@@ -1,3 +1,3 @@
 exports.get = (req, res) => {
-  res.render('randomContent', { link: '/try' });
+  res.render('randomContent', { backLink: '/try' });
 };
--- src/controllers/randomGift.js
+++ src/controllers/randomGift.js
@@ -1,3 +1,3 @@
 exports.get = (req, res) => {
-  res.render('randomGift', { link: '/' });
+  res.render('randomGift', { backLink: '/' });
 };
