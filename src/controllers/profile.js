6c1c152bdee79fec039843ac072206600d342b0b
Parissai <parissa.jamali@gmail.com>
2018-05-16 17:07:33 +0100

    add profile page

---
+++ src/controllers/profile.js
@@ -0,0 +1,10 @@
+const { getUser } = require('./../model/queries/getUser');
+
+exports.get = async (req, res) => {
+  if (req.session.username === req.params.username) {
+    const user = await getUser(req.session.username);
+    const userLikes = user.like;
+    res.render('profile', { userLikes });
+  }
+};
+
