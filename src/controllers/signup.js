4cbec2c7bea15ec8090fb13d0cd17799d0e077cc
isnotafunction <ivana.cosgrove@gmail.com>
2018-05-17 15:02:12 +0100

    fixes content

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,27 +1,21 @@
 const bcrypt = require('bcryptjs');
 const newUser = require('./../model/queries/createUser');
 
-const convertTime = time => {
-  let reversedTime = time
-    .split(':')
-    .reverse()
-    .join(' ');
-  time = '00 ' + reversedTime + ' * * 1-7';
-  return time;
-};
-
 exports.get = (req, res) => {
   res.render('signup', { backLink: '/' });
 };
 
 exports.post = (req, res) => {
-  let { username, password, wakeUpTime, like } = req.body;
-  const time = convertTime(wakeUpTime);
+  const {
+    username, password, like,
+  } = req.body;
   bcrypt
     .hash(password, 10)
-    .then(password => newUser.create({ username, password, time, like }))
+    .then(password => newUser.create({
+      username, password, like,
+    }))
     .then(() => res.render('thankYou', { user: username }))
-    .catch(err => {
+    .catch((err) => {
       if (err.message.includes('duplicate')) {
         res.render('signup', { err: 'This username is already taken!' });
       } else {
