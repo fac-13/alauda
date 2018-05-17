a1cac0af86f771230fbb1bd64ecf4b2dd9f9be9f
missKatiaPunter <katiapunter@gmail.com>
2018-05-17 11:31:30 +0100

    fixes syntax error Relates #15

--- src/controllers/getsubscribedcontent.js
+++ src/controllers/getsubscribedcontent.js
@@ -7,15 +7,15 @@ const query = 'inspiration';
 const newsUrl = `https://newsapi.org/v2/everything?q=${query}&totalResults=10&apiKey=${process.env.NEWS_KEY}`;
 
 var content = {
-    "Tech": [],
-    "Nature": [],
-    "Science": [],
-    "Travel": [],
-    "Psychology": [],
-    "Movies": [],
-    "Art": [],
-    "Books": []
-;
+    "Tech": ['js', 'python', 'sql'],
+    "Nature": ['birds', 'foxes', 'fish'],
+    "Science": ['dna', 'singularity', 'tihkal'],
+    "Travel": ['ussr', 'cuba', 'north korea'],
+    "Psychology": ['freud', 'zimbardo', 'not maslow'],
+    "Movies": ['tarantino', 'park chan wook', 'tarkovsky'],
+    "Art": ['picasso', 'ivi', 'isaac'],
+    "Books": ['js good bits', 'elquent js', 'war and peace']
+}
 /**
  * @param  {} url; make an API call and update content object
  */
@@ -23,11 +23,12 @@ const fetchApi = (url) => {
   fetch(url)
     .then(response => response.json())
     .then((json) => {
-        content = json;
+        // content = json;
+        writeDataToFile(content);
     });
 };
 
-writeDataToFile(content);
+
 
 /**
   * @param  {} data; Writes data that came from API calls into a json file that is stored for a day in the root folder.
@@ -43,7 +44,7 @@ function writeDataToFile(data) {
      * Runs every day at 00:00:00 AM and fetches content from News Api
 */
 const job = new CronJob({
-  cronTime: '00 34 15 * * 1-7',
+  cronTime: '00 28 11 * * 1-7',
   onTick() {
     fetchApi(newsUrl);
     console.log('Cron Job is being done')
--- src/controllers/userContent.js
+++ src/controllers/userContent.js
@@ -1,16 +1,29 @@
 const content = require('../../content.json');
 
-exports.get = (req, res) => {
+exports.get = async (req, res) => {
     if (req.session.length > 0) 
     {
         const { username, loggedIn } = req.session;
+        const user = await getUser(username);
         console.log(req.session);
+        console.log(user);
         console.log(content.articles[0].title);
         let art = content.articles;
         res.render('usercontent', { art });
     }
 };
 
+
+// exports.get = async (req, res) => {
+//     if (req.session.username === req.params.username) {
+//       const user = await getUser(req.session.username);
+//       const userLikes = user.like;
+//       res.render('profile', { userLikes });
+//     }
+//   };
+
+
+// const foundUser = await getUser(loginusername);
 
 // exports.get = (req, res) => {
 //   if (req.session.length > 0) {
