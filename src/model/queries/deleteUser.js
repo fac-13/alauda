c3bd4fdf1246d13af4fe314aef817673f24f6fc0
missKatiaPunter <katiapunter@gmail.com>
2018-05-17 19:11:37 +0100

    refactors delete user Realtes #15

--- src/model/queries/deleteUser.js
+++ src/model/queries/deleteUser.js
@@ -1,7 +1,6 @@
 const { User } = require('./../database/userSchema');
 
 async function deleteUser(data) {
-    console.log('You have reached delete user', data);
   return User.remove({username: data});
 }
 
