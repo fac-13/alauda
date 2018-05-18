2be86b26b8c2c9eea0c0be5589c43ee79adb9ade
missKatiaPunter <katiapunter@gmail.com>
2018-05-18 09:53:14 +0100

    refactors delete Relates #15

--- src/controllers/deleteProfile.js
+++ src/controllers/deleteProfile.js
@@ -1,14 +1,15 @@
 const { deleteUser } = require('./../model/queries/deleteUser');
 
 exports.get = async (req, res) => {
-  console.log('you have reached the delete profile', req.session);
   const { username } = req.session;
-  console.log(username);
   try {
     await deleteUser(username);
+    req.session = null;
     res.render('deleteProfile');
   } catch (err) {
-    console.log(err);
+    res.render('deleteProfile', {
+      errorMessage: 'User does not exist',
+    });
   }
 
 };
