1d29f41d1137975c2e82f76c12efeac27ca42647
Giulia <teggi.giulia@gmail.com>
2018-05-18 08:22:23 +0100

    Remove underline from initial

--- src/controllers/userContent.js
+++ src/controllers/userContent.js
@@ -11,7 +11,7 @@ exports.get = async (req, res) => {
       const user = await getUser(username);
       const likedContent = getUserContent(user.like);
       res.render('usercontent', {
-        likedContent, usercontent: true, username, initial,
+        likedContent, usercontent: true, backLink: '/login', username, initial,
       });
     } catch (err) {
       const error = err.message;
